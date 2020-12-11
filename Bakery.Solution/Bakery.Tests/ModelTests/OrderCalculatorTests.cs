d9611707572777e8950af01e30410984aefd0854
Joey3001 <josephnero111@gmail.com>
2020-12-11 13:39:59 -0800

    Stuck troubleshooting test#2

--- Bakery.Solution/Bakery.Tests/ModelTests/OrderCalculatorTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/OrderCalculatorTests.cs
@@ -12,5 +12,12 @@ namespace Bakery.Tests
       OrderCalculator newOrderCalculator = new OrderCalculator();
       Assert.AreEqual(typeof(OrderCalculator), newOrderCalculator.GetType());
     }
+    [TestMethod]
+    public void BreadCalculator_CalculateCostOfBreadOrderForEvenAmountOfLoafs_10()
+    {
+      OrderCalculator newOrderCalculator = new OrderCalculator();
+      BreadOrder newBreadOrder = new BreadOrder(4);
+      Assert.AreEqual(1, newOrderCalculator.BreadCalculator());
+    }
   }
 }
