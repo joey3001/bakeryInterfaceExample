14066ca3a35fecce4ce4491f86caa18b6ac46392
Joey3001 <josephnero111@gmail.com>
2020-12-11 10:17:54 -0800

    Create 1st test for Bread.cs. Good Fail

--- Bakery.Solution/Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/BreadTests.cs
@@ -3,5 +3,14 @@ using Bakery.Models;
 
 namespace Bakery.Tests
 {
-  
+  [TestClass]
+  public class BreadOrderTests
+  {
+    [TestMethod]
+    public void test()
+    {
+      BreadOrder newBreadOrder = new BreadOrder(5, 1);
+      Assert.AreEqual(typeof(string), newBreadOrder.GetType());
+    }
+  }
 }
--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -2,8 +2,27 @@ using System;
 
 namespace Bakery.Models
 {
-  public class Bread
+  public class BreadOrder
   {
+    private int _costPerLoaf;
+    public int CostPerLoaf 
+    {
+      get
+      {
+          return _costPerLoaf; 
+      }
+      set
+      {
+          _costPerLoaf = 5; 
+      }
+    }
+    public int NumberOfLoafs { get; set; }
 
+
+    public BreadOrder(int _costPerLoaf, int numberOfLoafs)
+    {
+      CostPerLoaf = _costPerLoaf; 
+      NumberOfLoafs = numberOfLoafs; 
+    }
   }
 }
