f1cce2c6ca9bc5d08f0d205b9e41c7a4f6ee8f82
Joey3001 <josephnero111@gmail.com>
2020-12-11 11:08:40 -0800

    Add feature to Bread.CS. Get good fail.

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -4,9 +4,22 @@ namespace Bakery.Models
 {
   public class BreadOrder
   {
+    private int _costPerLoaf;
+    public int CostPerLoaf 
+    {
+      get
+      {
+          return _costPerLoaf; 
+      }
+      set
+      {
+          _costPerLoaf = 5; 
+      }
+    }
     public int NumberOfLoafs { get; set; }
     public BreadOrder(int numberOfLoafs)
     {
+      CostPerLoaf = _costPerLoaf; 
       NumberOfLoafs = numberOfLoafs; 
     }
   }
